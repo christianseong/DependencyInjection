8157bbd5fc57b01eefdfaeea9f7a6339f70c7385
unknown <christian199@naver.com>
2019-12-14 11:51:43 +0900

    Dependency Injection

---
+++ spring/src/spring/di/entity/NewlecExam.java
@@ -0,0 +1,17 @@
+package spring.di.entity;
+
+public class NewlecExam implements Exam {
+	private int kor;
+	private int math;
+	private int eng;
+	private int com;
+	
+	@Override
+	public int total() {
+		return kor+eng+math+com;
+	}
+	@Override
+	public float avg() {
+		return total()/4.0f;
+	}
+}
